67f374e76d7631631d0b38ea82cf49279514d743
kvnmlr <kevin.mueller1@live.de>
2018-09-04 13:43:05 +0200

    added chartjs on analytics page

---
+++ src/components/analytics/ActivityChart.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "ActivityChart"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/analytics/RadarChart.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "RadarChart"
+    }
+</script>
+
+<style scoped>
+
+</style>
