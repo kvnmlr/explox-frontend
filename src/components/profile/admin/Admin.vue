c68f53fefe6dca99bfd803a2a7bfb6630ab82883
Kevin Müller <kevin.mueller1@live.de>
2018-11-22 15:25:38 +0100

    query routes and segmetns separately

--- src/components/profile/admin/Admin.vue
+++ src/components/profile/admin/Admin.vue
@@ -108,12 +108,17 @@
             this.activities = data.activities;
           }
         });
-        this.GET('dashboard/admin?routes=true&segments=true', (data, err) => {
+        this.GET('dashboard/admin?routes=true', (data, err) => {
           console.log(data);
           if (!err) {
             this.routes = data.routes;
-            this.segments = data.segments;
             this.creatorResults = data.creatorResults;
+          }
+        });
+        this.GET('dashboard/admin?segments=true', (data, err) => {
+          console.log(data);
+          if (!err) {
+            this.segments = data.segments;
           }
         });
       },
